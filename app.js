6d19b842509a46266030fb4c8dc0705d1ddd3d2b
s1monbengtsson <simonbengan8@gmail.com>
2022-10-15 18:44:36 +0200

    meta tags included, corrected lighthouse pointers

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+// Javascript for hamburger.
+// window.onload was used since my eventListener was read before my variables
+
 window.onload = function () {
 	const menu = document.querySelector('.navbar__btn');
 	const menuLinks = document.querySelector('.navbar__menu');
